f59cb7a707ec858934c4c5985e87a8a1f2b0d38c
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-06-14 19:00:12 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Premiersoft on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
